b7c2c591615a427a4e390ae862a66d5aed637367
Helcon Mabesa <helcon.mabesa@foxnews.com>
2016-06-17 13:54:01 -0400

    variable fix

--- src/numLiveUpdate.js
+++ src/numLiveUpdate.js
@@ -199,7 +199,7 @@
         var App = function(opts, callback) {
 
             // requires a callback function
-            if (!isFN(callback)) { return false; }
+            if (!isFn(callback)) { return false; }
 
             render(callback, {
                 val: parseFloat(opts.val),
